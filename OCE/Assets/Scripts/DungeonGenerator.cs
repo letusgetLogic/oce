fb3339f9bb81fac854877527014389936a2a1d1a
letusgetLogic <namnguyen.nn604@gmail.com>
2025-05-27 16:47:17 +0200

    Scripting DungeonGenerator
    Start: 15:45
    End: 16:45

--- OCE/Assets/Scripts/DungeonGenerator.cs
+++ OCE/Assets/Scripts/DungeonGenerator.cs
@@ -1,8 +1,7 @@
 using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEngine;
 
-public class WorldGenerator : MonoBehaviour
+public class DungeonGenerator : MonoBehaviour
 {
     public enum RoomType 
     {
@@ -30,17 +29,18 @@ public class WorldGenerator : MonoBehaviour
     private const int mapWidth = 9;
     private const int mapHeight = 8;
     private const int minEndRooms = 2;
-    private List<Vector2Int> endRooms = new();
+    private List<Vector2Int> endRoomsList;
     private Vector3 startPos;
     
     private void Awake()
     {
         startPos = new Vector3(mapWidth, 0.0f, mapHeight) * 0.5f * -1.0f * roomScale;
+        map = GenerateGridMap(2, ref endRoomsList);
     }
 
-    private int[,] GenerateGridMap(int level, ref List<Vector2Int> endRoom)
+    private int[,] GenerateGridMap(int level, ref List<Vector2Int> endRooms)
     {
-        endRoom = new();
+        endRoomsList = new();
         int roomNumbers = (int)(Random.Range(0, 2) + 5 + level * 2.6f);
 
         int[,] tempMap = new int[mapWidth, mapHeight];
@@ -60,13 +60,20 @@ public class WorldGenerator : MonoBehaviour
         do
         {
             // Regeln einbinden (LevelGeneration)
-
+            int[,] generateMap = GenerateLevel(tempMap, roomNumbers, ref endRooms);
 
             // Level validieren
+            isValidate = ValidateMap(generateMap, roomNumbers, endRooms, minEndRooms);
+
             // Ja = fertig
+            if (isValidate)
+                tempMap = generateMap;
+
             // nein = Wiederholung
+            iteration++;
         }
         while (!isValidate && iteration < maxIteration);
+        return tempMap;
     }
 
     private int[,] GenerateLevel(int[,] currentMap, int roomAmount, ref List<Vector2Int> endRooms)
@@ -82,7 +89,8 @@ public class WorldGenerator : MonoBehaviour
 
         int currentRooms = 1;
 
-        while (posToExpand.Count > 0) {
+        while (posToExpand.Count > 0)
+        {
             Vector2Int currentPos = posToExpand.Dequeue();
             Vector2Int[] posToCheck = new Vector2Int[]
                 {
@@ -104,16 +112,87 @@ public class WorldGenerator : MonoBehaviour
                         continue;
 
                     // 2. Bist du belegt => ja => Exit
-                    if (tempMap[toCheck.x, toCheck.y] != (int)RoomType.free) 
+                    if (tempMap[toCheck.x, toCheck.y] != (int)RoomType.free)
                         continue;
 
                     // 3. 50% Quit Chance
-                    if (Random.Range(0, 2) == 0)
+                    float rndPercent = UnityEngine.Random.Range(0.0f, 1.0f);
+                    if (rndPercent <= 0.5f)
                         continue;
 
                     // 4. Hast du 2 oder mehr Nachbarn? => ja => Exit
+                    int neighboursCount = GetNeighboursCount(tempMap, toCheck);
+                    if (neighboursCount > 1)
+                        continue;
+
+                    currentMap[toCheck.x, toCheck.y] = (int)RoomType.normal;
+                    posToExpand.Enqueue(toCheck);
+                    roomAmount++;
+                    addedRoom = true;
                 }
             }
+            if (!addedRoom)
+                endRooms.Add(currentPos);
+        }
+
+        return tempMap;
     }
 
+    private int GetNeighboursCount(int[,] map, Vector2Int pos)
+    {
+        int width = map.GetLength(0);
+        int height = map.GetLength(1);
+        int neighboursCount = 0;
+
+        Vector2Int[] posToCheck = new Vector2Int[]
+        {
+            pos + Vector2Int.up,
+            pos + Vector2Int.right,
+            pos + Vector2Int.down,
+            pos + Vector2Int.left,
+        };
+
+        for (int i = 0; i < posToCheck.Length; i++)
+        {
+            Vector2Int toCheck = posToCheck[i];
+            if (toCheck.x >= 0 && toCheck.x < width && toCheck.y >= 0 && toCheck.y < height)
+            {
+                if (map[toCheck.x, toCheck.y] != (int)RoomType.free)
+                {
+                    neighboursCount++;
+                }
+            }
+        }
+
+        return neighboursCount;
+    }
+
+    private bool ValidateMap(int[,] checkMap, int countOfRooms, List<Vector2Int> listOfEndrooms, int endroomCount)
+    {
+        // haben wir genug Endräume
+        if (listOfEndrooms.Count < endroomCount)
+            return false;
+
+        // haben wir genug Räume
+        int width = checkMap.GetLength(0);
+        int height = checkMap.GetLength(1);
+        int count = 0;
+
+        foreach (int item in checkMap)
+        {
+            // ggf. redundant
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (checkMap[x, y] != (int)RoomType.free)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count >= countOfRooms;
+    }
 }
